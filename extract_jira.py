8c53354c7c4d4bab98fc075d0a015f30a10463d9
Pakatip Kasai <kasai.pakatip@gmail.com>
2025-04-03 16:41:37 +0900

    Update extract_jira.py

--- extract_jira.py
+++ extract_jira.py
@@ -7,17 +7,14 @@ API_TOKEN = os.getenv("JIRA_API_TOKEN")
 JIRA_URL = "https://pkasai.atlassian.net"
 PROJECT_KEY = "DGRC"
 
-url = f"{JIRA_URL}/rest/api/3/search"
 auth = HTTPBasicAuth(EMAIL, API_TOKEN)
 headers = { "Accept": "application/json" }
 
-params = {
-    "jql": "project=DGRC",
-    "maxResults": 100,
-    "fields": "*all"
-}
+url = f"{JIRA_URL}/rest/api/3/project/search"
+response = requests.get(url, headers=headers, auth=auth)
 
-response = requests.get(url, headers=headers, auth=auth, params=params)
+
+response = requests.get(url, headers=headers, auth=auth)
 
 print(response.status_code)
 print(response.json())
